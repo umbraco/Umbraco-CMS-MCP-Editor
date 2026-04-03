77e68e3585daa678c57d949017405b2673d846fb
Phil Whittaker <pjw@umbraco.dk>
2026-04-03 20:45:43 +0100

    feat: register media, media-management, blueprint collections

--- src/collections.ts
+++ src/collections.ts
@@ -22,8 +22,18 @@
 import contentCollection from "./umbraco-api/tools/content/index.js";
 import publishingCollection from "./umbraco-api/tools/publishing/index.js";
 import versioningCollection from "./umbraco-api/tools/versioning/index.js";
+import mediaCollection from "./umbraco-api/tools/media/index.js";
+import mediaManagementCollection from "./umbraco-api/tools/media-management/index.js";
+import blueprintCollection from "./umbraco-api/tools/blueprint/index.js";
 
-export const collections = [contentCollection, publishingCollection, versioningCollection];
+export const collections = [
+  contentCollection,
+  publishingCollection,
+  versioningCollection,
+  mediaCollection,
+  mediaManagementCollection,
+  blueprintCollection,
+];
 
 export { allModes, allModeNames } from "./config/mode-registry.js";
 export { allSliceNames } from "./config/slice-registry.js";
